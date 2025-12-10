fb55d0ba5735c70af3d2057f6fe78768a34be553
Arman Mulla <armanbashirmulla.15@gmail.com>
2025-12-11 00:16:22 +0530

    data

---
+++ Html css/css/main.js
@@ -0,0 +1,32 @@
+// main. js
+
+const subMenus =
+document.querySelectorAll(".sub-menu"),
+btns = document. querySelectorAll("button");
+
+const reset = ( ) => {
+btns. forEach(btn =>
+btn.classList. remove("active"));
+
+subMenus. forEach(menu =>
+(menu. style.height = 0) );
+
+const openSubmenu = element => {
+reset();
+element. classList.add("active");
+
+const sibling = element. nextElementSibling;
+const ul = sibling. querySelector("ul");
+
+if (sibling.clientHeight === 0) {
+sibling. style.height = '${ul.clientHeight}px';
+} else {
+sibling.style.height = 0;
+element. classList. remove("active");
+}
+}
+const gotoPage = element => {
+reset( );
+element.classList.add("active");
+};
+}
